1e770afa411d3e6994320429a2d9ed4a374a9149
Jeanre Swanepoel <jeanres@icloud.com>
2023-03-08 12:46:22 +0200

    add tmux

--- home/shell/default.nix
+++ home/shell/default.nix
@@ -1,44 +1,48 @@
 { pkgs, inputs, system, ... }:
 
 {
-    programs.zsh = {
-        enable = true;
-        shellAliases = {
-            vi = "nvim";
-            vim = "nvim";
-            vimdiff = "nvim -d";
-        };
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      vi = "nvim";
+      vim = "nvim";
+      vimdiff = "nvim -d";
     };
+  };
 
-    programs.git = {
-        enable = true;
-        userName = "Jeanre Swanepoel";
-        userEmail = "jeanres@icloud.com";
-        extraConfig = {
-            init = {
-                defaultBranch = "main";
-            };
-            merge = {
-                ff = "only";
-            };
-            pull = {
-                rebase = true;
-            };
-            rebase = {
-                autoSquash = true;
-                autoStash = true;
-            };
-            commit = {
-                verbose = true;
-            };
-            url = {
-                "ssh://git@github.com/" = {
-                    insteadOf = [ "github:" "gh:" ];
-                };
-            };
+  programs.tmux = {
+    enable = true;
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Jeanre Swanepoel";
+    userEmail = "jeanres@icloud.com";
+    extraConfig = {
+      init = {
+        defaultBranch = "main";
+      };
+      merge = {
+        ff = "only";
+      };
+      pull = {
+        rebase = true;
+      };
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+      };
+      commit = {
+        verbose = true;
+      };
+      url = {
+        "ssh://git@github.com/" = {
+          insteadOf = [ "github:" "gh:" ];
         };
+      };
     };
+  };
 
-    programs.starship.enable = true;
-    programs.direnv.enable = true;
+  programs.starship.enable = true;
+  programs.direnv.enable = true;
 }
