7097579247cb1f72726476c0f82f354e952d9e89
Jeanre Swanepoel <jeanres@icloud.com>
2023-03-08 12:39:29 +0200

    tab updates

--- home/neovim/init.lua
+++ home/neovim/init.lua
@@ -4,18 +4,14 @@ require('plugins')
 vim.opt.number = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
-
 vim.opt.incsearch = true
 vim.opt.hlsearch = true
-
 vim.opt.wrap = false
-
--- Global status line
 vim.opt.laststatus = 3
 
 vim.opt.termguicolors = true
