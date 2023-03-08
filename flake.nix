54cb4fabef944fefed28de9e380088529c15507d
Jeanre Swanepoel <jeanres@icloud.com>
2023-03-08 12:37:46 +0200

    cleanup

--- flake.nix
+++ flake.nix
@@ -1,58 +1,57 @@
 {
-  description = "Jeanre's darwin system";
+    description = "Jeanre's darwin system";
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+        darwin.url = "github:lnl7/nix-darwin/master";
+        darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    darwin.url = "github:lnl7/nix-darwin/master";
-    darwin.inputs.nixpkgs.follows = "nixpkgs";
-
-   home-manager = {
-      url = "github:nix-community/home-manager/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-
-  outputs = { self, darwin, nixpkgs, home-manager }: {
-    darwinConfigurations = {
-	    "JeanreWnderLabz"  = darwin.lib.darwinSystem {
-            system = "aarch64-darwin";
-            modules = [
-	            ./hosts/MacBook-Pro
- 	            home-manager.darwinModules.home-manager {
-                    home-manager.useGlobalPkgs = true;
-                    home-manager.useUserPackages = true;
-                    home-manager.users."jeanre.swanepoel" = { pkgs, ... }: {
-                        imports = [
-		                    ./home/shell
-		                    ./home/neovim
-				    ./home/terminal
-                        ];
-                        home.stateVersion = "22.11";
-                    };
-                }
-            ];
-
-          };
-    	"JeanresPersonal" = darwin.lib.darwinSystem {
-          system = "aarch64-darwin";
-          modules = [
-              ./hosts/MacBook-Pro
-                  home-manager.darwinModules.home-manager
-                  {
-                      home-manager.useGlobalPkgs = true;
-                      home-manager.useUserPackages = true;
-                      home-manager.users."jeanre.swanepoel" = { pkgs, ... }: {
-                          imports = [
-                              ./home/shell
-                                  ./home/neovim
-./home/terminal
-                          ];
-                          home.stateVersion = "22.11";
-                      };
-                  }
-          ];
+        home-manager = {
+            url = "github:nix-community/home-manager/master";
+            inputs.nixpkgs.follows = "nixpkgs";
         };
-
     };
-};
+
+    outputs = { self, darwin, nixpkgs, home-manager }: {
+        darwinConfigurations = {
+            "JeanreWnderLabz"  = darwin.lib.darwinSystem {
+                system = "aarch64-darwin";
+                modules = [
+                    ./hosts/darwin
+                        home-manager.darwinModules.home-manager {
+                            home-manager.useGlobalPkgs = true;
+                            home-manager.useUserPackages = true;
+                            home-manager.users."jeanre.swanepoel" = { pkgs, ... }: {
+                                imports = [
+                                    ./home/shell
+                                        ./home/neovim
+                                        ./home/terminal
+                                ];
+                                home.stateVersion = "22.11";
+                            };
+                        }
+                ];
+
+            };
+            "JeanresPersonal" = darwin.lib.darwinSystem {
+                system = "aarch64-darwin";
+                modules = [
+                    ./hosts/darwin
+                        home-manager.darwinModules.home-manager
+                        {
+                            home-manager.useGlobalPkgs = true;
+                            home-manager.useUserPackages = true;
+                            home-manager.users."jeanre.swanepoel" = { pkgs, ... }: {
+                                imports = [
+                                    ./home/shell
+                                        ./home/neovim
+                                        ./home/terminal
+                                ];
+                                home.stateVersion = "22.11";
+                            };
+                        }
+                ];
+            };
+
+        };
+    };
 }
