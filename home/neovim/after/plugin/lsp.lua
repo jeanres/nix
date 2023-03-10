f4937a7071ff4c4edbeeb5f2bc7e3d858b0d0e01
Jeanre Swanepoel <jeanres@icloud.com>
2023-03-10 11:04:28 +0200

    setup java

--- home/neovim/after/plugin/lsp.lua
+++ home/neovim/after/plugin/lsp.lua
@@ -6,92 +6,76 @@ keymap.nnoremap(']d', vim.diagnostic.goto_next, {silent=true})
 keymap.nnoremap('<leader>d', vim.diagnostic.open_float, {silent=true})
 
 local organize_imports = function (client, timeout_ms)
-    if not client.server_capabilities.codeActionProvider then
-        return
-    end
+  if not client.server_capabilities.codeActionProvider then
+    return
+  end
 
-    local params = vim.lsp.util.make_range_params(nil, "utf-16")
-    params.context = { only = { "source.organizeImports" } }
-    local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
-    for _, res in pairs(result or {}) do
-        for _, r in pairs(res.result or {}) do
-            if r.edit then
-                vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
-            else
-                vim.lsp.buf.execute_command(r.command)
-            end
-        end
+  local params = vim.lsp.util.make_range_params(nil, "utf-16")
+  params.context = { only = { "source.organizeImports" } }
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, 3000)
+  for _, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        vim.lsp.util.apply_workspace_edit(r.edit, "utf-16")
+      else
+        vim.lsp.buf.execute_command(r.command)
+      end
     end
+  end
 end
 
 local on_attach = function (client, bufnr)
-    local nmap = keymap.bind("n", {noremap=true, silent=true, buffer=bufnr})
+  local nmap = keymap.bind("n", {noremap=true, silent=true, buffer=bufnr})
 
-    nmap("gD", vim.lsp.buf.declaration)
-    nmap("gd", vim.lsp.buf.definition)
-    nmap("K", vim.lsp.buf.hover)
-    nmap("gi", vim.lsp.buf.implementation)
-    nmap("<C-k>", vim.lsp.buf.signature_help)
-    nmap("<leader>wa", vim.lsp.buf.add_workspace_folder)
-    nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder)
-    nmap("<leader>wl", function()
-        print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    end)
-    nmap("<leader>D", vim.lsp.buf.type_definition)
-    nmap("<leader>rn", vim.lsp.buf.rename)
-    nmap("<leader>ca", vim.lsp.buf.code_action)
-    nmap("gr", vim.lsp.buf.references)
+  nmap("gD", vim.lsp.buf.declaration)
+  nmap("gd", vim.lsp.buf.definition)
+  nmap("K", vim.lsp.buf.hover)
+  nmap("gi", vim.lsp.buf.implementation)
+  nmap("<C-k>", vim.lsp.buf.signature_help)
+  nmap("<leader>wa", vim.lsp.buf.add_workspace_folder)
+  nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder)
+  nmap("<leader>wl", function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end)
+  nmap("<leader>D", vim.lsp.buf.type_definition)
+  nmap("<leader>rn", vim.lsp.buf.rename)
+  nmap("<leader>ca", vim.lsp.buf.code_action)
+  nmap("gr", vim.lsp.buf.references)
 
-    vim.api.nvim_create_autocmd({"BufWritePre"}, {
-        buffer = bufnr,
-        callback = function ()
-            organize_imports(client, 1001)
-            vim.lsp.buf.formatting_sync(nil, 1000)
-        end,
-    })
+  vim.api.nvim_create_autocmd({"BufWritePre"}, {
+    buffer = bufnr,
+    callback = function ()
+      organize_imports(client, 1001)
+      vim.lsp.buf.formatting_sync(nil, 1000)
+    end,
+  })
 end
 
 config["solargraph"].setup({
-    on_attach = on_attach,
+  on_attach = on_attach,
 })
 
 config["gopls"].setup({
-    on_attach = on_attach,
+  on_attach = on_attach,
 })
 
 config["terraformls"].setup({
-    on_attach = on_attach,
+  on_attach = on_attach,
 })
 
 config["rnix"].setup({
-    on_attach = on_attach,
+  on_attach = on_attach,
 })
 
 config["hls"].setup({
-    on_attach = on_attach,
+  on_attach = on_attach,
 })
 
 config["tsserver"].setup({
-    on_attach = on_attach,
+  on_attach = on_attach,
 })
 
+
 config["jdtls"].setup({
-  cmd = {
-    "java",
-    "-Declipse.application=org.eclipse.jdt.ls.core.id1",
-    "-Dosgi.bundles.defaultStartLevel=4",
-    "-Declipse.product=org.eclipse.jdt.ls.core.product",
-    "-Dlog.protocol=true",
-    "-Dlog.level=ALL",
-    "-Xms1g",
-    "--add-modules=ALL-SYSTEM",
-    "--add-opens",
-    "java.base/java.util=ALL-UNNAMED",
-    "--add-opens",
-    "java.base/java.lang=ALL-UNNAMED",
-    "-jar",
-    vim.fn.glob("~/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_*.jar"),
-    "-configuration ~/.local/share/nvim/mason/packages/jdtls/config_mac",
-  },
   on_attach = on_attach,
 })
