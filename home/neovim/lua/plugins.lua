998245471d5b6fbd3f3ac4458d30d5059a263088
Jeanre Swanepoel <jeanres@icloud.com>
2023-03-12 07:39:53 +0200

    more config

--- home/neovim/lua/plugins.lua
+++ home/neovim/lua/plugins.lua
@@ -1,35 +1,46 @@
 local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-    packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
+  packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+  vim.cmd [[packadd packer.nvim]]
 end
 
 return require('packer').startup(function(use)
-    use("wbthomason/packer.nvim")
-    use("nvim-lua/plenary.nvim")
-    use("nvim-telescope/telescope.nvim")
-    use "williamboman/mason.nvim"
-    use {
-	    'nvim-treesitter/nvim-treesitter',
-	    run = function()
-		    local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-		    ts_update()
-	    end,
-    }
-    use('folke/tokyonight.nvim')
-    use('nvim-lualine/lualine.nvim')
-    use('github/copilot.vim')
-    use("neovim/nvim-lspconfig")
-    -- Snippets
-    use("L3MON4D3/LuaSnip")
-    -- Completion
-    use("hrsh7th/cmp-nvim-lsp")
-    use("hrsh7th/cmp-buffer")
-    use("hrsh7th/cmp-path")
-    use("saadparwaiz1/cmp_luasnip")
-    use("hrsh7th/nvim-cmp")
-    if packer_bootstrap then
-        require("packer").sync()
-    end
+  use("wbthomason/packer.nvim")
+  use("nvim-lua/plenary.nvim")
+  use("nvim-telescope/telescope.nvim")
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use "williamboman/mason.nvim"
+  use 'ThePrimeagen/git-worktree.nvim'
+  use 'tpope/vim-fugitive'
+  use {
+    'nvim-tree/nvim-tree.lua',
+    requires = {
+      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+  }
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  }
+  use('folke/tokyonight.nvim')
+  use('nvim-lualine/lualine.nvim')
+  use('github/copilot.vim')
+  use("neovim/nvim-lspconfig")
+  -- Snippets
+  use("L3MON4D3/LuaSnip")
+  -- Completion
+  use("hrsh7th/cmp-nvim-lsp")
+  use("hrsh7th/cmp-buffer")
+  use("hrsh7th/cmp-path")
+  use("saadparwaiz1/cmp_luasnip")
+  use("hrsh7th/nvim-cmp")
+  use 'mfussenegger/nvim-jdtls'
+  if packer_bootstrap then
+    require("packer").sync()
+  end
 end)
